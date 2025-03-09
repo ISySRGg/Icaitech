fc098c5e0af53422620a6c98446db2e49b398fe7
moez-rd <slow9ie@gmail.com>
2025-03-09 12:12:59 +0700

    update submission

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -134,14 +134,14 @@ export default function Page() {
           </article>
         </div>
       </section>
-      <section 
+      <section
         id="ImportantDate"
         className="container flex flex-col items-center pt-20">
         <h2 className="text-center text-3xl font-semibold md:text-5xl">
           Important Date
         </h2>
 
-        <div className="mt-6 grid grid-cols-3 gap-4">
+        <div className="mt-6 grid md:grid-cols-3 gap-4">
           {importantDate.map((item, i) => (
             <div key={i} className="flex flex-col gap-2 rounded border p-8">
               <p>{item.name}</p>
@@ -278,7 +278,7 @@ export default function Page() {
             height="200"
             className="h-28 w-auto"
           />
-         
+
         </div>
       </section>
       <section className="container pt-20">
@@ -295,7 +295,7 @@ export default function Page() {
           className="mt-6 w-full"
         ></iframe>
       </section>
-     
+
     </>
   )
 }
