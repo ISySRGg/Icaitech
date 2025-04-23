48d3ae0f3851faa30812273753fdc377742c1fe8
aaddeeis <adesapitri13@gmail.com>
2025-04-23 07:58:53 +0700

    ksks

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -21,8 +21,8 @@ export default function Header() {
              <Image
               src="/assets/rmh kita logo.png"
               alt="icaitech"
-              height={20}
-              width={20}
+              height={30}
+              width={30}
               className="h-10 md:h-12 w-fit"
             />
             <Image
