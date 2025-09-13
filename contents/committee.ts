68740d40658717831e1b45557c2f632aa6016ee1
moez-rd <slow9ie@gmail.com>
2025-09-13 09:30:26 +0700

    update-reviewer

--- contents/committee.ts
+++ contents/committee.ts
@@ -197,64 +197,276 @@ export const organizingComittee: Committee[] = [
 ]
 
 export const reviewers: Committee[] = [
-  // {
-  //   name: "Julian Supardi",
-  //   affiliation: "Universitas Sriwijaya",
-  // },
-  // {
-  //   name: "Sukemi",
-  //   affiliation: "Universitas Sriwijaya",
-  // },
-  // {
-  //   name: "Dian Palupi",
-  //   affiliation: "Universitas Sriwijaya",
-  // },
-  // {
-  //   name: "Mira Afrina",
-  //   affiliation: "Universitas Sriwijaya",
-  // },
-  // {
-  //   name: "Abdiansyah",
-  //   affiliation: "Universitas Sriwijaya",
-  // },
-  // {
-  //   name: "Ermatita",
-  //   affiliation: "Universitas Sriwijaya",
-  // },
-  // {
-  //   name: "Ali Ibrahim",
-  //   affiliation: "Universitas Sriwijaya",
-  // },
-  // {
-  //   name: "Ahmad Heryanto",
-  //   affiliation: "Universitas Sriwijaya",
-  // },
-  // {
-  //   name: "Ahmad Zarkasi",
-  //   affiliation: "Universitas Sriwijaya",
-  // },
-  // {
-  //   name: "Ken Dhita Tania",
-  //   affiliation: "Universitas Sriwijaya",
-  // },
-  // {
-  //   name: "Fathoni",
-  //   affiliation: "Universitas Sriwijaya",
-  // },
-  // {
-  //   name: "Qurhanul Rizqie",
-  //   affiliation: "Universitas Sriwijaya",
-  // },
-  // {
-  //   name: "Anggina Primanita",
-  //   affiliation: "Universitas Sriwijaya",
-  // },
-  // {
-  //   name: "Hadipurnawan Satria",
-  //   affiliation: "Universitas Sriwijaya",
-  // },
-  // {
-  //   name: "Annisa Darmawahyuni",
-  //   affiliation: "Universitas Sriwijaya",
-  // },
+  {
+    name: "Dr Renan Prasta Jenie, STP, MT",
+    affiliation: "Binawan University",
+  },
+  {
+    name: "Prof. Dr.Ir. Mewati Ayub, M.T.",
+    affiliation: "Universitas Kristen Maranatha",
+  },
+  {
+    name: "Dr. Wiwin Suwarningsih, M.T.",
+    affiliation: "BRIN",
+  },
+  {
+    name: "Dr. Dany Eka Saputra, M.T.",
+    affiliation: "Bina Nusantara University",
+  },
+  {
+    name: "Dr. Dede Kurniadi, S.Kom.,M.Kom.",
+    affiliation: "Institut Teknologi Garut",
+  },
+  {
+    name: "Dr. Adiyasa Nurfalah, M.T.",
+    affiliation: "Bandung Institute of Technology",
+  },
+  {
+    name: "Erly Krisnanik",
+    affiliation: "Universitas Pembangunan Nasional Veteran Jakarta",
+  },
+  {
+    name: "I Wayan Widi Pradnyana, S.Kom, MTI",
+    affiliation: "Universitas Pembangunan Nasional Veteran Jakarta",
+  },
+  {
+    name: "Dr. Heni Sulistiani, M.Kom.",
+    affiliation: "Universitas Teknokrat Indonesia",
+  },
+  {
+    name: "Dr. Ir. Aradea, S.T., M.T.",
+    affiliation: "Universitas Siliwangi",
+  },
+  {
+    name: "Dr. Saluky, S.Si, M.Kom",
+    affiliation: "UIN Siber Syekh Nurjati Cirebon",
+  },
+  {
+    name: "Mindit Eriyadi, M.T",
+    affiliation: "Telkom University",
+  },
+  {
+    name: "Tio Dharmawan, S.Kom., M.Kom",
+    affiliation: "Universitas Jember",
+  },
+  {
+    name: "Abram Setyo Prabowo, S.Kom.,M.Cs",
+    affiliation: "Universitas Bina Nusantara",
+  },
+  {
+    name: "Adhe Lingga Dewi, S.Si., M.Si",
+    affiliation: "Computer Science, School of Computer Science, Binus University, Jakarta, Indonesia",
+  },
+  {
+    name: "Adri Senen, ST., MT",
+    affiliation: "Institut Teknologi PLN",
+  },
+  {
+    name: "Imam Ahmad, M.Kom.",
+    affiliation: "Universitas Teknokrat Indonesia",
+  },
+  {
+    name: "Dr. Chaerur Rozikin., S. Kom., M. Kom",
+    affiliation: "Universitas singaperbangsa Karawang",
+  },
+  {
+    name: "Dr. Mariana Purba, M.Kom",
+    affiliation: "Universitas Sjakhyakirti",
+  },
+  {
+    name: "Dr. (Cand) Zaenal Abidin, S.Si., S.Kom., M.T.",
+    affiliation: "Universitas Teknokrat Indonesia",
+  },
+  {
+    name: "Aprianti Nanda Sari, S.T., M.Kom.",
+    affiliation: "Politeknik Negeri Bandung",
+  },
+  {
+    name: "Dr. Erliyan Redy Susanto, M.Kom",
+    affiliation: "Universitas Teknokrat Indonesia",
+  },
+  {
+    name: "Ridwan Setiawan, S.T., M.Kom., MTA., ITS",
+    affiliation: "Institut Teknologi Garut",
+  },
+  {
+    name: "Artika Arista, S.Kom, MMSI",
+    affiliation: "Universitas Pembangunan Nasional Veteran Jakarta/Universiti Malaya",
+  },
+  {
+    name: "Dr. Cand. Daniel Yeri Kristiyanto, S.Kom., M.Kom., M.Si",
+    affiliation: "Telkom University",
+  },
+  {
+    name: "Ucta Pradema Sanjaya M.Kom",
+    affiliation: "Universitas Ngudi Waluyo",
+  },
+  {
+    name: "Welan Mauli Angguna, S.Psi., M.Si",
+    affiliation: "Politeknik Negeri Sriwijaya",
+  },
+  {
+    name: "Ernando Rizki Dalimunthe, S.T.,M.T.",
+    affiliation: "Universitas Teknokrat Indonesia",
+  },
+  {
+    name: "Astika Ulfah Izzati, S.T., M.A.B",
+    affiliation: "Politeknik negeri Sriwijaya",
+  },
+  {
+    name: "Dr. Radiant Victor Imbar. S.Kom., M.T.",
+    affiliation: "Maranatha Christian University",
+  },
+  {
+    name: "Ir. Samsurizal, ST., MT., IPM",
+    affiliation: "Institut Teknologi PLN",
+  },
+  {
+    name: "Retnani Latifah, S.Kom., M.Kom",
+    affiliation: "Universitas Muhammadiyah Jakarta",
+  },
+  {
+    name: "Wina Permana Sari, S.T., M.Kom.",
+    affiliation: "Binus",
+  },
+  {
+    name: "Dr. Handrie Noprisson, ST., M.Kom",
+    affiliation: "Universitas Dian Nusantara Jakarta",
+  },
+  {
+    name: "Ni Putu Linda Santiari, S.Kom., M.Kom.",
+    affiliation: "Institut Teknologi dan Bisnis STIKOM Bali",
+  },
+  {
+    name: "Andreas Widjaja, S.Si., M.Sc., Ph.D., MCE.",
+    affiliation: "Universitas Kristen Maranatha",
+  },
+  {
+    name: "Dr Puji Rahayu, M.Kom",
+    affiliation: "UMB",
+  },
+  {
+    name: "Andhika Octa Indarso, S.Kom., M.MSI",
+    affiliation: "UPN Veteran Jakarta",
+  },
+  {
+    name: "Sai Kalyan Reddy Pentaparthi, MS",
+    affiliation: "ST Engineering iDirect Inc, USA",
+  },
+  {
+    name: "Ashutosh Rana",
+    affiliation: "Cherryroad Technologies",
+  },
+  {
+    name: "SHUBHAM SRIVASTAVA",
+    affiliation: "University of Washington",
+  },
+  {
+    name: "Sai Kumar Bitra",
+    affiliation: "JNTU India",
+  },
+  {
+    name: "Jeesmon Jacob",
+    affiliation: "Amazon / Colorado Technical University",
+  },
+  {
+    name: "Sai Kumar Bitra",
+    affiliation: "AT&T",
+  },
+  {
+    name: "Sai Prasad Mukala",
+    affiliation: "Info Keys Inc",
+  },
+  {
+    name: "Sree Priyanka Uppu",
+    affiliation: "Amazon Web Servcies(AWS)",
+  },
+  {
+    name: "Kiran Kumar Lekkala, M.C.A., B.Sc. (Computer Science)",
+    affiliation: "Puget Sound Energy",
+  },
+  {
+    name: "Viswakanth Ankireddi, Bachelor of Engineering (Honours)",
+    affiliation: "INTEL",
+  },
+  {
+    name: "Shubham Srivastava",
+    affiliation: "Amazon",
+  },
+  {
+    name: "Shashank Chaudhary",
+    affiliation: "Fractal Analytics",
+  },
+  {
+    name: "Parthsarthi Rawat",
+    affiliation: "GameChanger Media",
+  },
+  {
+    name: "Rohit Kumar Ravula, MS",
+    affiliation: "Intra-Cellular Therapies, Inc",
+  },
+  {
+    name: "I Komang Ari Mogi, S.Kom., M.Kom.",
+    affiliation: "Universitas Udayana",
+  },
+  {
+    name: "Hastie Audytra, S.Kom., M.T.",
+    affiliation: "Universitas Nahdlatul Ulama Sunan Giri",
+  },
+  {
+    name: "Nirma Ceisa Santi,M.Kom",
+    affiliation: "Universitas Nahdlatul Ulama Sunan Giri Bojonegoro",
+  },
+  {
+    name: "Mr. Rajeev Samuel Devadas",
+    affiliation: "IBM Corporation",
+  },
+  {
+    name: "Shiva Kumar Chinnnam",
+    affiliation: "Rialtic Inc",
+  },
+  {
+    name: "Sandeep Kamadi",
+    affiliation: "Wilmington University, Delaware, USA",
+  },
+  {
+    name: "Yovi Pratama, S.Kom, MT",
+    affiliation: "University Teknologi Malaysia",
+  },
+  {
+    name: "Rajkumar Govindaswamy Subbian",
+    affiliation: "Goden Bear Insurance",
+  },
+  {
+    name: "Mr. Venkaiah Chirumavilla",
+    affiliation: "Texas Department of Agriculture",
+  },
+  {
+    name: "Mr. Kalyan Pavan Kumar Madicharla",
+    affiliation: "Amazon Web Services",
+  },
+  {
+    name: "Dr. Ankita Saxena, Sr. Product Manager",
+    affiliation: "Amazon",
+  },
+  {
+    name: "Praveen Kumar Myakala",
+    affiliation: "Independent AI Researcher",
+  },
+  {
+    name: "Anup Kagalkar",
+    affiliation: "Product Technical Expert, Independent Researcher",
+  },
+  {
+    name: "Shubham Gupta",
+    affiliation: "Noblesoft Solutions Inc, San Antonio, Texas, USA",
+  },
+  {
+    name: "Satish Kabade",
+    affiliation: "Independent Researcher",
+  },
+  {
+    name: "Sameeksha Gupta",
+    affiliation: "Meta Platforms",
+  },
 ]
