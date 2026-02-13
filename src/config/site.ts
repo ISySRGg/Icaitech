b91996e1c01a951ef9bdedaf3fea6b47b5df08cc
moez-rd <rahmat.ge4@gmail.com>
2026-02-13 14:43:54 +0700

    Fix URL path for "Important Date" in site configuration for consistency

--- src/config/site.ts
+++ src/config/site.ts
@@ -12,7 +12,7 @@ export const siteConfig = {
       children: [
         {
           label: "Important Date",
-          href: "/ImportantDate",
+          href: "/important-date",
         },
         {
           label: "Conference Tracks",
@@ -35,7 +35,7 @@ export const siteConfig = {
     { label: "Committee", href: "/committee" },
     { label: "Contact", href: "/contact" },
   ],
-  submitlink: "https://edas.info/N34962", //ini untuk button submit 
+  submitlink: "https://edas.info/N34962", //ini untuk button submit
   callForReviewerLink:
     "https://docs.google.com/forms/d/e/1FAIpQLSeuU6ue76n9xIQDamsXH09AKt6_iXQ-gbB5Sw1TFVNnDTpzQQ/viewform",
 }
